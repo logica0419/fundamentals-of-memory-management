46bda56dbf2b51334a5d374cace2d493e8e533f6
Takuto Nagami <logica0419@gmail.com>
2025-10-23 11:28:40 -0700

    reset timer after initialization

--- eg-filter/filter_test.go
+++ eg-filter/filter_test.go
@@ -3,11 +3,13 @@ package main
 import "testing"
 
 func BenchmarkFilterBefore(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		var list targetList = make([]int, 100)
 		for i := 0; i < 100; i++ {
 			list[i] = i
 		}
+
+		b.ResetTimer()
 
 		_ = list.FilterBefore(func(i int) bool {
 			return i%2 == 0
@@ -16,11 +18,13 @@ func BenchmarkFilterBefore(b *testing.B) {
 }
 
 func BenchmarkFilterAfter(b *testing.B) {
-	for range b.N {
+	for b.Loop() {
 		var list targetList = make([]int, 100)
 		for i := 0; i < 100; i++ {
 			list[i] = i
 		}
+
+		b.ResetTimer()
 
 		_ = list.FilterAfter(func(i int) bool {
 			return i%2 == 0
